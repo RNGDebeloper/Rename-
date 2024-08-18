894f8ffec631f758df932950a1a002935cbf4be6
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-08-18 15:48:01 +0530

    Update F_Sub.py
    dine

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -8,7 +8,7 @@ from helper.utils import not_subscribed
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    buttons = [[InlineKeyboardButton('📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢', url=client.invitelink)]]
+    buttons = [[ InlineKeyboardButton('📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢', url=client.invitelink) ]]
     text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ **"
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
           
