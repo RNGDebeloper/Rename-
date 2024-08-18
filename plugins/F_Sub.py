98891fc099459cabfd7c94ac6d19495f820a839b
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-08-18 15:33:19 +0530

    Update F_Sub.py
    update

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -8,8 +8,8 @@ from helper.utils import not_subscribed
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    buttons = [[ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink) ],[InlineKeyboardButton('📢 𝙹𝚘𝚒𝚗 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢', url='https://t.me/Toonami_India')]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ **"
+    buttons = [[ InlineKeyboardButton(text="📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢", url=https://t.me/+wm7HnsM4mM8xNmFl),InlineKeyboardButton('📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢', url=client.invitelink)]]
+    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ **"
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
           
 
