a22addfe94327d7a292bef4c270d5e8dbca74544
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-08-18 12:37:39 +0530

    Update config.py
    Rename 4G bot added

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = os.environ.get("API_ID", "24260003")
 
 API_HASH = os.environ.get("API_HASH", "a7fb5ac26f0e0a5742c43675fdec8f54")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6640714840:AAFOoiK76PdagSD27se4pHHZKcJmvLdAfDc") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7500440012:AAH17XIVDzfQtRS5onwHw3d8tSydbwkGlNs") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Uchiha_Developer") 
 
@@ -25,7 +25,7 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://Lazy:Lazy@cluster0.zigg8lw.mong
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/1485f2a9ef06e7369e20a.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6335438828').split()]
 
