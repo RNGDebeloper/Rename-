0bf9a4d6a4ec074d9a5b633d2616c1582f3a04c4
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-08-18 14:43:46 +0530

    Update txt.py
    updated renaming h

--- helper/txt.py
+++ helper/txt.py
@@ -4,7 +4,7 @@
 
 class mr(object):
     PROGRESS_BAR = """\n
-╭━━━━❰ VJ Bot Renaming... ❱━➣
+╭━━━━❰ Bot Renaming... ❱━➣
 ┣⪼ 🗂️ : {1} | {2}
 ┣⪼ ⏳️ : {0}%
 ┣⪼ 🚀 : {3}/s
@@ -14,7 +14,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 My Name is : {}
-├👑 CHANNEL : <a href=https://t.me/VJ_Bots>VJ Bots</a> 
+├👑 CHANNEL : <a href=https://t.me/Uchiha_Developer>Uchiha Developer</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
