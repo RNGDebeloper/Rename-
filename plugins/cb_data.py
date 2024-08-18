891ebb2ceaab3b4e7c3565876c6dbd2db0b72ad9
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-08-18 12:41:27 +0530

    Update cb_data.py
    w

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -39,7 +39,7 @@ async def doc(bot,update):
      ms = await update.message.edit("⚠️__**Please wait...**__\n__Downloading file to my server...__")
      c_time = time.time()
      try:
-     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "\n⚠️__**Please wait...**__\n\n😈 **VJ Hack in progress...**",  ms, c_time   ))
+     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "\n⚠️__**Please wait...**__\n\n😈 **Work in progress...**",  ms, c_time   ))
      except Exception as e:
      	await ms.edit(e)
      	return 
